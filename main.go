c2c5db6e047519270219615d67402ce03b9ab733
Aaron Lee <aaronlee232@gmail.com>
2026-03-14 17:36:43 -0500

    Refactored frontend routes

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/aaronlee232/redis-gui-tester/api"
-	ui "github.com/aaronlee232/redis-gui-tester/ui/templates"
+	"github.com/aaronlee232/redis-gui-tester/ui"
 )
 
 func main() {
@@ -16,7 +15,7 @@ func main() {
 	// Start Server
 	addr := ":3000"
 	apiRouter := api.NewAPIRouter()
-	frontendRouter := NewFrontendRouter()
+	frontendRouter := ui.NewFrontendRouter()
 
 	mainRouter := http.NewServeMux()
 	mainRouter.Handle("/api/", http.StripPrefix("/api", apiRouter))
@@ -26,19 +25,4 @@ func main() {
 	if err := http.ListenAndServe(addr, mainRouter); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func NewFrontendRouter() *http.ServeMux {
-	// Serve frontend pages
-	component := ui.Layout("Redis GUI Tester")
-
-	r := http.NewServeMux()
-
-	// Serve static files (CSS, JS) under /static/
-	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("ui/static"))))
-	r.Handle("/", templ.Handler(component))
-
-	return r
-	// fmt.Println("Listening on :3000")
-	// log.Fatal(http.ListenAndServe(":3000", nil))
 }
